c2758203875d0d156f0cd6a7fe2ee14ee2d8813a
Tomas <tomaserobino@gmail.com>
2024-07-23 21:47:12 +0200

    basic ProjectSection done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.module.css'
 import Header from "./Header.tsx";
 import styles from "./App.module.css"
+import ProjectSection from "./ProjectSection.tsx";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <ProjectSection />
     </div>
   )
 }
---
+++ src/ProjectSection.tsx
@@ -0,0 +1,9 @@
+import styles from "./ProjectSection.module.css"
+
+export default function ProjectSection() {
+  return (
+    <div className={styles.section}>
+
+    </div>
+  )
+}
