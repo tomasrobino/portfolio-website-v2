b4efa7e38f10ce393d1450a493bbf0276db36d0e
Tomas <tomaserobino@gmail.com>
2024-07-25 11:12:17 +0200

    work on projects

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,9 +1,9 @@
 import styles from "./Project.module.css";
 import React from "react";
 
-export default function Project(props: { title: string, image: string, description: string }) {
+export default function Project(props: { alignment?: boolean , title: string, image: string, description: string }) {
   return (
-    <div className={styles.proj}>
+    <div className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
       <img src={props.image} alt={props.title} className={styles.img}/>
       <div className={styles.titleNDesc}>
         <h2>{props.title}</h2>
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,9 +1,15 @@
 import styles from "./ProjectSection.module.css"
+import Project from "./Project.tsx";
 
 export default function ProjectSection() {
   return (
     <div className={styles.section}>
-
+      <h1 className={styles.title}>Projects</h1>
+      <div className={styles.group}>
+        <Project image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <Project alignment={true} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <Project image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+      </div>
     </div>
   )
 }
