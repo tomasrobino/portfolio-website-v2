402bd1db7ea2d05ffaede565f5a57d61fa351a92
Tomas <tomaserobino@gmail.com>
2024-07-25 17:23:56 +0200

    css transforms done

--- src/Project.tsx
+++ src/Project.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Project = React.forwardRef((props: { alignment?: boolean , title: string, image: string, description: string }, ref: React.ForwardedRef<HTMLDivElement>) => {
   return (
     <div ref={ref} className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
-      <img src={props.image} alt={props.title} className={styles.img}/>
+      <img src={props.image} alt={props.title} className={`${styles.img} ${props.alignment ? styles.rightImg : ""}`}/>
       <div className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
         <h2>{props.title}</h2>
         <p>{props.description}</p>
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,11 +1,53 @@
 import styles from "./ProjectSection.module.css"
 import Project from "./Project.tsx";
-import React, {RefObject, useRef} from "react";
+import React, {RefObject, useEffect, useRef} from "react";
 
 export default function ProjectSection() {
   const ref1: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   const ref2: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   const ref3: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref1.current && ref2.current && ref3.current) {
+      const first = ref1.current;
+      const second = ref2.current;
+      const third = ref3.current;
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          entry.time
+          if (entry.isIntersecting) {
+            entry.target.classList.remove(styles.contract);
+            entry.target.classList.add(styles.expand);
+          }
+        });
+      }, {threshold: 0.5});
+
+      const secondObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          entry.time
+          if (!entry.isIntersecting) {
+            entry.target.classList.remove(styles.expand);
+            entry.target.classList.add(styles.contract);
+          }
+        });
+      }, { threshold: 0.3});
+
+      observer.observe(first);
+      observer.observe(second);
+      observer.observe(third);
+
+      secondObserver.observe(first);
+      secondObserver.observe(second);
+      secondObserver.observe(third);
+
+      return () => {
+        if (first) observer.unobserve(first);
+        if (second) observer.unobserve(second);
+        if (third) observer.unobserve(third);
+      }
+    }
+  }, [ref1, ref2, ref3]);
 
   return (
     <div className={styles.section}>
