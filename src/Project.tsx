1b9712b1f54d6c9a3ec67c69c4a42607ef2f54e6
Tomas <tomaserobino@gmail.com>
2024-07-25 11:48:43 +0200

    styling for project done

--- src/Project.tsx
+++ src/Project.tsx
@@ -5,7 +5,7 @@ export default function Project(props: { alignment?: boolean , title: string, im
   return (
     <div className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
       <img src={props.image} alt={props.title} className={styles.img}/>
-      <div className={styles.titleNDesc}>
+      <div className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
         <h2>{props.title}</h2>
         <p>{props.description}</p>
       </div>
