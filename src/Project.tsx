7f6f0da27e52d8d26dd5c00d50239d5c282032e5
Tomas <tomaserobino@gmail.com>
2024-07-25 16:02:51 +0200

    ref to project div element done

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,9 +1,9 @@
 import styles from "./Project.module.css";
 import React from "react";
 
-export default function Project(props: { alignment?: boolean , title: string, image: string, description: string }) {
+const Project = React.forwardRef((props: { alignment?: boolean , title: string, image: string, description: string }, ref: React.ForwardedRef<HTMLDivElement>) => {
   return (
-    <div className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
+    <div ref={ref} className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
       <img src={props.image} alt={props.title} className={styles.img}/>
       <div className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
         <h2>{props.title}</h2>
@@ -11,4 +11,6 @@ export default function Project(props: { alignment?: boolean , title: string, im
       </div>
     </div>
   )
-}
+})
+
+export default Project;
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,14 +1,19 @@
 import styles from "./ProjectSection.module.css"
 import Project from "./Project.tsx";
+import React, {RefObject, useRef} from "react";
 
 export default function ProjectSection() {
+  const ref1: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const ref2: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const ref3: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
   return (
     <div className={styles.section}>
       <h1 className={styles.title}>Projects</h1>
       <div className={styles.group}>
-        <Project image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <Project alignment={true} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <Project image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <Project ref={ref1} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <Project ref={ref2} alignment={true} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <Project ref={ref3} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
       </div>
     </div>
   )
