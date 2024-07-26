ed571dfa1b2fcc4d7be88b15c470eba6a2c18dd3
Tomas <tomaserobino@gmail.com>
2024-07-26 20:37:43 +0200

    styled Footer and About icon for mobile

--- src/About.tsx
+++ src/About.tsx
@@ -10,18 +10,12 @@ export default function About() {
     <section id="about" className={styles.section}>
       <h1 className={`${styles.title} ${styles.fadeIn}`}>I'm <span className={styles.highlight}>Tomas</span>,</h1>
       <h1 className={`${styles.subtitle} ${styles.fadeIn}`}> a web developer seeking to create <span className={styles.highlight}>great</span> user experiences</h1>
-      <svg
-        onClick={handleClick}
-        className={styles.icon}
-        height="30vh"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z"
-          fill="white"
-        />
+      <svg onClick={handleClick} className={styles.icon} height="30vh" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+          <path d="M17 9.5L12 14.5L7 9.5" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round"></path>
+        </g>
       </svg>
     </section>
   )
