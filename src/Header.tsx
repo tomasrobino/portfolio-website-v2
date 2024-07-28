38d5b7b994f3aacbc85d4805b2dc7eab9a878eed
Tomas <tomaserobino@gmail.com>
2024-07-28 15:14:52 +0200

    switched header a tags for p tags and href for scrollIntoView()

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,22 @@
 import styles from "./Header.module.css"
 
 export default function Header() {
+  function handleAbout() {
+    const id =document.getElementById("about")
+    if (id) id.scrollIntoView({ behavior: "smooth" })
+  }
+
+  function handleProjects() {
+    const id =document.getElementById("projects")
+    if (id) id.scrollIntoView({ behavior: "smooth" })
+  }
+
   return(
     <header>
       <a href={'/'} className={styles.headerLink} style={{ fontSize: "1.3em" }}>Tomas Robino</a>
       <div className={styles.headerGroup}>
-        <a href={'#about'} className={styles.headerLink}>about</a>
-        <a href={'#projects'} className={styles.headerLink}>projects</a>
+        <p onClick={handleAbout} className={styles.headerLink}>about</p>
+        <p onClick={handleProjects} className={styles.headerLink}>projects</p>
       </div>
     </header>
   )
