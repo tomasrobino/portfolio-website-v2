37aedd6d7c7fb815f2ccc85392f9ce13e4aad916
Tomas <tomaserobino@gmail.com>
2024-07-28 15:24:49 +0200

    split ProjectElement Link up

--- src/ProjectElement.tsx
+++ src/ProjectElement.tsx
@@ -4,15 +4,16 @@ import {Link} from "react-router-dom";
 
 const ProjectElement = React.forwardRef((props: { alignment?: boolean , id: string, title: string, image: string, description: string }, ref: React.ForwardedRef<HTMLDivElement>) => {
   return (
-    <Link to={`/projects/${props.id}`} className={styles.link}>
       <div ref={ref} className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
-        <img src={props.image} alt={props.title} className={`${styles.img} ${props.alignment ? styles.rightImg : ""}`}/>
+        <Link to={`/projects/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={`${styles.img} ${props.alignment ? styles.rightImg : ""}`}/></Link>
         <div className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
+          <Link to={`/projects/${props.id}`} className={styles.link}>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+          </Link>
         </div>
       </div>
-    </Link>
+
   )
 })
 
