29c70f41f60dc9954987fccd0e759ed5a19fc593
Tomas <tomaserobino@gmail.com>
2024-08-02 15:29:48 +0200

    added fade to titleNDesc

--- src/ProjectElement.tsx
+++ src/ProjectElement.tsx
@@ -1,12 +1,19 @@
 import styles from "./ProjectElement.module.css";
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 const ProjectElement = React.forwardRef((props: { alignment?: boolean , id: string, title: string, image: string, description: string }, ref: React.ForwardedRef<HTMLDivElement>) => {
+  useEffect(( ) => {
+    AOS.init();
+  }, []);
+
+
   return (
       <div ref={ref} className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
         <Link to={`/projects/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={`${styles.img} ${props.alignment ? styles.rightImg : ""}`}/></Link>
-        <div className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
+        <div data-aos={props.alignment ? "fade-left" : "fade-right"} className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
           <Link to={`/projects/${props.id}`} className={styles.link}>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
