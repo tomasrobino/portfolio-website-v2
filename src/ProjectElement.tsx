6f7088d6ecf38dbbfe0a1eb5519a65b6575eb667
Tomas <tomaserobino@gmail.com>
2024-08-02 17:01:28 +0200

    switched animations to aos, styled titleNDesc

--- src/ProjectElement.tsx
+++ src/ProjectElement.tsx
@@ -4,23 +4,22 @@ import {Link} from "react-router-dom";
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
-const ProjectElement = React.forwardRef((props: { alignment?: boolean , id: string, title: string, image: string, description: string }, ref: React.ForwardedRef<HTMLDivElement>) => {
+
+export default function ProjectElement(props: { alignment?: boolean , id: string, title: string, image: string, description: string }) {
   useEffect(( ) => {
     AOS.init();
   }, []);
 
 
   return (
-      <div ref={ref} className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
-        <Link to={`/projects/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={`${styles.img} ${props.alignment ? styles.rightImg : ""}`}/></Link>
-        <div data-aos={props.alignment ? "fade-left" : "fade-right"} className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
-          <Link to={`/projects/${props.id}`} className={styles.link}>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-          </Link>
-        </div>
+    <div data-aos="zoom-in" className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
+      <Link to={`/projects/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={styles.img}/></Link>
+      <div data-aos={props.alignment ? "fade-left" : "fade-right"} data-aos-offset="500" className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
+        <Link to={`/projects/${props.id}`} className={styles.link}>
+          <h1 className={styles.title}>{props.title}</h1>
+          <p className={`${styles.desc} ${props.alignment? styles.descRight : ""}`}>{props.description}</p>
+        </Link>
       </div>
+    </div>
   )
-})
-
-export default ProjectElement;
+}
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,61 +1,15 @@
 import styles from "./ProjectSection.module.css"
 import ProjectElement from "./ProjectElement.tsx";
-import React, {RefObject, useEffect, useRef} from "react";
 
 export default function ProjectSection() {
-  const ref1: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-  const ref2: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-  const ref3: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (ref1.current && ref2.current && ref3.current) {
-      const first = ref1.current;
-      const second = ref2.current;
-      const third = ref3.current;
-
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          entry.time
-          if (entry.isIntersecting) {
-            entry.target.classList.remove(styles.contract);
-            entry.target.classList.add(styles.expand);
-          }
-        });
-      }, {threshold: 0.5});
-
-      const secondObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          entry.time
-          if (!entry.isIntersecting) {
-            entry.target.classList.remove(styles.expand);
-            entry.target.classList.add(styles.contract);
-          }
-        });
-      }, { threshold: 0.3});
-
-      observer.observe(first);
-      observer.observe(second);
-      observer.observe(third);
-
-      secondObserver.observe(first);
-      secondObserver.observe(second);
-      secondObserver.observe(third);
-
-      return () => {
-        if (first) observer.unobserve(first);
-        if (second) observer.unobserve(second);
-        if (third) observer.unobserve(third);
-      }
-    }
-  }, [ref1, ref2, ref3]);
 
   return (
     <div className={styles.section} id="projects">
       <h1 className={styles.title}>Projects</h1>
       <div className={styles.group}>
-        <ProjectElement ref={ref1} id="1" image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <ProjectElement ref={ref2} id="2" alignment={true} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <ProjectElement ref={ref3} id="3" image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <ProjectElement id="1" image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <ProjectElement id="2" alignment={true} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        <ProjectElement id="3" image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
       </div>
     </div>
   )
