c8d225a22fa8770dbe481e302d51eba99657d61c
Tomas <tomaserobino@gmail.com>
2024-08-02 18:17:21 +0200

    projects styling for mobile

--- src/ProjectElement.tsx
+++ src/ProjectElement.tsx
@@ -22,7 +22,7 @@ export default function ProjectElement(props: { languages: string[], alignment?:
   return (
     <div data-aos="zoom-in" className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
       <Link to={`/projects/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={styles.img}/></Link>
-      <div data-aos={props.alignment ? "fade-left" : "fade-right"} data-aos-offset="500" className={`${styles.titleNDesc} ${props.alignment ? styles.justify : ""}`}>
+      <div data-aos={props.alignment ? "fade-left" : "fade-right"} data-aos-offset="500" className={`${styles.titleNDesc} ${props.alignment ? styles.titleNDescRight : ""}`}>
         <Link to={`/projects/${props.id}`} className={styles.link}>
           <h1 className={styles.title}>{props.title}</h1>
           <p className={`${styles.desc} ${props.alignment? styles.descRight : ""}`}>{props.description}</p>
