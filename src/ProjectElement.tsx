452724f13771a0abecc8f4f8eefb8c46ae1a9d1a
Tomas <tomaserobino@gmail.com>
2024-08-12 18:13:40 +0200

    switched project link for github

--- src/ProjectElement.tsx
+++ src/ProjectElement.tsx
@@ -23,7 +23,7 @@ export default function ProjectElement(props: { languages: string[], alignment?:
     <div data-aos="zoom-in-down" className={`${styles.project} ${props.alignment ? styles.alignment : ""}`}>
       <Link to={`https://github.com/tomasrobino/${props.id}`} className={styles.link}><img src={props.image} alt={props.title} className={styles.img}/></Link>
       <div data-aos={props.alignment ? "fade-left" : "fade-right"} data-aos-delay="300" className={`${styles.titleNDesc} ${props.alignment ? styles.titleNDescRight : ""}`}>
-        <Link to={`/projects/${props.id}`} className={styles.link}>
+        <Link to={`https://github.com/tomasrobino/${props.id}`} className={styles.link}>
           <h1 className={styles.title}>{props.title}</h1>
           <p className={`${styles.desc} ${props.alignment? styles.descRight : ""}`}>{props.description}</p>
         </Link>
